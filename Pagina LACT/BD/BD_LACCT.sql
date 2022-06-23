aa0356cda229b6c99e0cf3594273e477014edfd8
DESTROLLER616 <104311024+DESTROLLER616@users.noreply.github.com>
2022-06-23 11:46:12 -0500

    version 2
    se agregaron mas tablas a la BD

--- Pagina LACT/BD/BD_LACCT.sql
+++ Pagina LACT/BD/BD_LACCT.sql
@@ -31,3 +31,53 @@ CREATE TABLE conductor(
 	id_vehiculo int,
 	salario decimal(5,2),
 	fecha_contracion date);
+
+CREATE TABLE almacenista(
+	id int NOT NULL,
+	nombre varchar(20) NOT NULL,
+	apellido_p varchar(20) NOT NULL,
+	apellido_m varchar(20) NOT NULL,
+	telefono varchar(10),
+	direccion varchar(50) NOT NULL,
+	id_ruta int,
+	id_vehiculo int,
+	salario decimal(5,2),
+	fecha_contracion date;
+)
+
+CREATE TABLE vehiculo(
+	id int NOT NULL,
+	marca varchar(15) NOT NULL,
+	modelo varchar(20) NOT NULL,
+	matricula varchar(10) NOT NULL,
+	carga int NOT NULL,
+	estado varchar NOT NULL);
+
+CREATE TABLE ruta(
+	id int NOT NULL,
+	destino varchar NOT NULL);
+
+CREATE TABLE pedido(
+	id int NOT NULL,
+	id_cliente int NOT NULL,
+	id_conductor int NOT NULL,
+	estado varchar(15),
+	subtotal decimal(10,2));
+
+CREATE TABLE manejo_pedido(
+	id int NOT NULL,
+	id_pedido int NOT NULL,
+	id_producto int NOT NULL,
+	fecha_creacion date,
+	cantidad int NOT NULL,
+	fecha_entrega date,
+	subtotal decimal(10,2));
+
+CREATE TABLE producto(
+	id int NOT NULL,
+	stock int NOT NULL,
+	precio decimal (10,2),
+	imagen image,
+	cantidad smallint NOT NULL,
+	descripcion varchar(40),
+)
